284edf746ad30972f12cbbce213215aedcea46ea
Linus Färnstrand <faern@faern.net>
2017-11-21 10:15:20 +0100

    Generate bindings to SCDynamicStore

--- system-configuration-sys/src/lib.rs
+++ system-configuration-sys/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+
+extern crate core_foundation_sys;
+
+pub mod dynamic_store {
+    include!(concat!(env!("OUT_DIR"), "/SCDynamicStore.rs"));
 }
