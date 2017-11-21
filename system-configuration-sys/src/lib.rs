b2e1011340d9f91e20a0fd0c920718f6b35a60c4
Linus Färnstrand <faern@faern.net>
2017-11-21 10:15:20 +0100

    Generate bindings to SCDynamicStore

--- system-configuration-sys/src/lib.rs
+++ system-configuration-sys/src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright 2017 Amagicom AB.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+
+extern crate core_foundation_sys;
+
+pub mod dynamic_store {
+    include!(concat!(env!("OUT_DIR"), "/SCDynamicStore.rs"));
 }
