eb90a7d35f7c40da80bffb4470d0fa5c2fcdafac
Linus Färnstrand <faern@faern.net>
2017-11-24 09:23:16 +0100

    Add SCDynamicConfiguration wrapper

---
+++ system-configuration/src/dynamic_store.rs
@@ -0,0 +1,101 @@
+// Copyright 2017 Amagicom AB.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use core_foundation::base::TCFType;
+use core_foundation::boolean::CFBoolean;
+use core_foundation::dictionary::{CFDictionary, CFDictionaryRef};
+use core_foundation::string::CFString;
+use core_foundation_sys::base::{CFRelease, kCFAllocatorDefault};
+use core_foundation_sys::propertylist::CFPropertyListRef;
+
+use system_configuration_sys::dynamic_store::*;
+
+use std::ptr;
+
+/// Access to the key-value pairs in the dynamic store of a running system.
+pub struct SCDynamicStore(SCDynamicStoreRef);
+
+impl Drop for SCDynamicStore {
+    fn drop(&mut self) {
+        unsafe { CFRelease(self.as_CFTypeRef()) }
+    }
+}
+
+impl_TCFType!(SCDynamicStore, SCDynamicStoreRef, SCDynamicStoreGetTypeID);
+
+impl SCDynamicStore {
+    /// Creates a new session used to interact with the dynamic store maintained by the System
+    /// Configuration server.
+    pub fn create<S: Into<CFString>>(name: S) -> Self {
+        let cf_name = name.into();
+        unsafe {
+            let store = SCDynamicStoreCreate(
+                kCFAllocatorDefault,
+                cf_name.as_concrete_TypeRef(),
+                None,
+                ptr::null_mut(),
+            );
+            SCDynamicStore::wrap_under_create_rule(store)
+        }
+    }
+
+    /// Creates a new session used to interact with the dynamic store maintained by the System
+    /// Configuration server. Uses [`SCDynamicStoreCreateWithOptions`] underneath and sets
+    /// `kSCDynamicStoreUseSessionKeys` to true.
+    ///
+    /// [`SCDynamicStoreCreateWithOptions`]: https://developer.apple.com/documentation/systemconfiguration/1437818-scdynamicstorecreatewithoptions?language=objc
+    pub fn create_with_session_keys<S: Into<CFString>>(name: S) -> Self {
+        let cf_name = name.into();
+        unsafe {
+            let store_options = CFDictionary::from_CFType_pairs(&[
+                (
+                    CFString::wrap_under_create_rule(kSCDynamicStoreUseSessionKeys),
+                    CFBoolean::true_value(),
+                ),
+            ]);
+            let store = SCDynamicStoreCreateWithOptions(
+                kCFAllocatorDefault,
+                cf_name.as_concrete_TypeRef(),
+                store_options.as_concrete_TypeRef(),
+                None,
+                ptr::null_mut(),
+            );
+            SCDynamicStore::wrap_under_create_rule(store)
+        }
+    }
+
+    /// If the given key exists in the store, the associated value is returned.
+    pub fn get<S: Into<CFString>>(&self, key: S) -> Option<CFDictionary> {
+        let cf_key = key.into();
+        unsafe {
+            let dict_ref =
+                SCDynamicStoreCopyValue(self.as_concrete_TypeRef(), cf_key.as_concrete_TypeRef());
+            if dict_ref != ptr::null() {
+                Some(CFDictionary::wrap_under_create_rule(
+                    dict_ref as *const _ as CFDictionaryRef,
+                ))
+            } else {
+                None
+            }
+        }
+    }
+
+    /// Sets the value of the given key. Overwrites existing values.
+    /// Returns `true` on success, false on failure.
+    pub fn set<S: Into<CFString>>(&self, key: S, value: &CFDictionary) -> bool {
+        let cf_key = key.into();
+        let success = unsafe {
+            SCDynamicStoreSetValue(
+                self.as_concrete_TypeRef(),
+                cf_key.as_concrete_TypeRef(),
+                value.as_concrete_TypeRef() as CFPropertyListRef,
+            )
+        };
+        success != 0
+    }
+}
--- system-configuration/src/lib.rs
+++ system-configuration/src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use]
+extern crate core_foundation;
+extern crate core_foundation_sys;
+extern crate system_configuration_sys;
+
+pub mod dynamic_store;
