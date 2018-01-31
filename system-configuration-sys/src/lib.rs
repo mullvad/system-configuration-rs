f3ed82df7be0a69ba5cf9b6d4fb619bd00ab2218
Linus Färnstrand <faern@faern.net>
2018-01-31 21:34:58 +0100

    Add some initial README/docs

--- system-configuration-sys/src/lib.rs
+++ system-configuration-sys/src/lib.rs
@@ -6,6 +6,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! Low level bindings to the Apple [SystemConfiguration] framework. Generated with bindgen.
+//! For a safe, higher level, API, check out the [`system-configuration`] crate.
+//!
+//! [SystemConfiguration]: https://developer.apple.com/documentation/systemconfiguration?language=objc
+//! [`system-configuration`]: https://crates.io/crates/system-configuration
+
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
--- system-configuration/src/lib.rs
+++ system-configuration/src/lib.rs
@@ -6,6 +6,17 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! # SystemConfiguration bindings
+//!
+//! This crate is a high level binding to the Apple [SystemConfiguration] framework. For low level
+//! FFI bindings, check out the [`system-configuration-sys`] crate.
+//!
+//! This crate only implements a small part of the [SystemConfiguration] framework so far. If you
+//! need a yet unimplemented part, feel free to submit a pull request!
+//!
+//! [SystemConfiguration]: https://developer.apple.com/documentation/systemconfiguration?language=objc
+//! [`system-configuration-sys`]: https://crates.io/crates/system-configuration-sys
+
 #[macro_use]
 extern crate core_foundation;
 extern crate system_configuration_sys;
