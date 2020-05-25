9377ac37e0ff4643e8f69e131077717d792a5747
Emīls Piņķis <emils@mullvad.net>
2020-05-25 11:39:03 +0100

    Add SCNetworkInterface

--- system-configuration-sys/src/network_configuration.rs
+++ system-configuration-sys/src/network_configuration.rs
@@ -188,6 +188,8 @@ extern "C" {
 
     pub static kSCNetworkInterfaceTypeBond: CFStringRef;
 
+    pub static kSCNetworkInterfaceTypeBridge: CFStringRef;
+
     pub static kSCNetworkInterfaceTypeEthernet: CFStringRef;
 
     pub static kSCNetworkInterfaceTypeFireWire: CFStringRef;
