647991be9d91478160df898038a4a5335a80f7cd
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-11 18:12:00 +0100

    all STILL gens nicely...

--- generate_genscript.py
+++ generate_genscript.py
@@ -24,6 +24,13 @@ class Item(TypedDict):
     binding_path_var: str
     binding_name: str
 
+    header_path_line: str
+    binding_path_line: str
+
+
+def grab(ix: List[Item], k: str) -> Item:
+    return [i for i in ix if i["pure_camel_case"] == k][0]
+
 
 def header_path_line(i: Item) -> str:
     return i["header_path_var"] + "=\"${SC_HEADER_PATH}/" + i["header_name"] + "\""
@@ -33,16 +40,45 @@ def binding_path_line(i: Item) -> str:
     return i["binding_path_var"] + "=\"${SC_BINDING_PATH}/" + i["binding_name"] + "\""
 
 
+def bindgen_big_item(i: Item) -> str:
+    return fr"""echo "Generating bindings for ${i['header_path_var']}"
+bindgen \
+    "${{BINDGEN_COMMON_ARGUMENTS[@]}}" \
+    --allowlist-function "SCPreferences.*" \
+    --blocklist-type "(__)?CF.*" \
+    --blocklist-type "Boolean" \
+    --blocklist-type "dispatch_queue_[ts]" \
+    --blocklist-type "(AuthorizationOpaqueRef|__SCPreferences)" \
+    --raw-line "use core::ffi::c_void;" \
+    --raw-line "use core_foundation_sys::array::CFArrayRef;" \
+    --raw-line "use core_foundation_sys::base::{{Boolean, CFIndex, CFAllocatorRef, CFTypeID}};" \
+    --raw-line "use core_foundation_sys::data::CFDataRef;" \
+    --raw-line "use core_foundation_sys::string::CFStringRef;" \
+    --raw-line "use core_foundation_sys::propertylist::CFPropertyListRef;" \
+    --raw-line "use core_foundation_sys::runloop::CFRunLoopRef;" \
+    --raw-line "" \
+    --raw-line "use crate::dispatch_queue_t;" \
+    --raw-line "" \
+    --raw-line "pub type AuthorizationOpaqueRef = c_void;" \
+    --raw-line "pub type __SCPreferences = c_void;" \
+    -o ${i['binding_path_var']} \
+    ${i['header_path_var']} -- \
+    -I$SDK_PATH/usr/include \
+    -F$FRAMEWORK_PATH
+
+cleanup_binding ${i['binding_path_var']}"""
+
+
 def header_path_block(ix: List[Item]) -> str:
-    return "\n".join([header_path_line(i) for i in ix])
+    return "\n".join([i["header_path_line"] for i in ix])
 
 
 def binding_path_block(ix: List[Item]) -> str:
-    return "\n".join([binding_path_line(i) for i in ix])
+    return "\n".join([i["binding_path_line"] for i in ix])
 
 
 # ---- data processing ----
-pure_cc_str = """CaptiveNetwork
+pure_camel_case_str = """CaptiveNetwork
 DHCPClientPreferences
 SCDynamicStore
 SCDynamicStoreCopyDHCPInfo
@@ -59,22 +95,31 @@ SCSchemaDefinitions
 SystemConfiguration"""
 
 # pure cc + header name -> contaner
-pure_cc = pure_cc_str.split('\n')
-items = [Item(pure_cc=s, pure_sc="", header_name=s + ".h", header_path_var="",
-              binding_path_var="", binding_name="") for s in pure_cc]
+pure_camel_case = pure_camel_case_str.split('\n')
+items = [Item(pure_camel_case=s, pure_snake_case="", header_name=s + ".h", header_path_var="",
+              binding_path_var="", binding_name="", header_path_line="", binding_path_line="") for s in pure_camel_case]
 
 # generate pure sc + binding name -> container
-items = [Item(i, pure_sc=i["pure_cc"].replace("SC", "")) for i in items]
-items = [Item(i, pure_sc=to_snake_case(i["pure_sc"])) for i in items]
-items = [Item(i, binding_name=i["pure_sc"] + ".rs") for i in items]
+items = [Item(i, pure_snake_case=i["pure_camel_case"].replace("SC", ""))
+         for i in items]
+items = [Item(i, pure_snake_case=to_snake_case(i["pure_snake_case"]))
+         for i in items]
+items = [Item(i, binding_name=i["pure_snake_case"] + ".rs") for i in items]
 
 # genrate path vars
-items = [Item(i, header_path_var=i["pure_sc"].upper() + "_HEADER_PATH",
-              binding_path_var=i["pure_sc"].upper() + "_BINDING_PATH") for i in items]
+items = [Item(i, header_path_var=i["pure_snake_case"].upper() + "_HEADER_PATH",
+              binding_path_var=i["pure_snake_case"].upper() + "_BINDING_PATH") for i in items]
 
-# GEN blocks
+# generate path lines
+items = [Item(i, header_path_line=header_path_line(
+    i), binding_path_line=binding_path_line(i)) for i in items]
+
+# ---- final output ----
 print("### header_path_block".upper())
 print(header_path_block(items), "\n")
 
 print("### binding_path_block".upper())
-print(binding_path_block(items))
+print(binding_path_block(items), "\n")
+
+print("### bindgens_block".upper())
+print(bindgen_big_item(grab(items, "SCPreferences")))
